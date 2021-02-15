2f3df2d85de028088cc743af9f59687334fcdb98
J.Fırat <61869832+JFASOF@users.noreply.github.com>
2021-02-15 20:55:19 +0300

    Update yer_Servis.dart

--- lib/src/harita_servis/yer_Servis.dart
+++ lib/src/harita_servis/yer_Servis.dart
@@ -3,7 +3,7 @@ import 'dart:convert' as convert;
 import 'package:ildonor/src/harita_servis/harita_Model.dart';
 
 class PlacesService {
-  final key = 'AIzaSyDqgv2AkmoyQOGKOBwP3244IA93alRQ7kc';
+  final key = 'YOUR_API_KEY';
   final url="https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=41.0025984,29.2225024&type=hospital&rankby=distance&key=AIzaSyDqgv2AkmoyQOGKOBwP3244IA93alRQ7kc";
   Future<List<Place>> getPlaces(double lat, double lng) async {
     var response = await http.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=$lat,$lng&type=hospital&rankby=distance&key=$key'
